3a80568311979d333bd1f642df4222ea7402690c
itzptr <149391552+itzptr@users.noreply.github.com>
2025-11-17 18:22:59 +0000

    Student Management placeholder page Rebase

---
+++ src/Jahoot.Display/Lecturer Views/StudentManagement.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace Jahoot.Display.Lecturer_Views
+{
+    /// <summary>
+    /// Interaction logic for StudentManagement.xaml
+    /// </summary>
+    public partial class StudentManagement : UserControl
+    {
+        public StudentManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
