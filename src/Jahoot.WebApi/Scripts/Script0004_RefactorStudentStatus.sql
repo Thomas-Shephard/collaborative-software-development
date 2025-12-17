78ec42c7dd924a66b199f52b71b5b23be140e62c
Thomas Shephard <thomas@thomas-shephard.com>
2025-12-17 20:40:35 +0000

    Fix migration script

--- src/Jahoot.WebApi/Scripts/Script0004_RefactorStudentStatus.sql
+++ src/Jahoot.WebApi/Scripts/Script0004_RefactorStudentStatus.sql
@@ -3,12 +3,7 @@ ALTER TABLE Student ADD COLUMN is_approved BOOL NOT NULL DEFAULT FALSE;
 
 -- Migrate existing data
 UPDATE Student SET is_approved = TRUE WHERE account_status = 'active';
-
--- For students that were disabled, disable the user account
-UPDATE User 
-JOIN Student ON User.user_id = Student.user_id 
-SET User.is_disabled = TRUE 
-WHERE Student.account_status = 'disabled';
+UPDATE User JOIN Student ON User.user_id = Student.user_id SET User.is_disabled = TRUE WHERE Student.account_status = 'disabled';
 
 -- Drop the old column
 ALTER TABLE Student DROP COLUMN account_status;
